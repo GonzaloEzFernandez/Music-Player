42d1490f651cd78d527775bcabe26586d8e43175
Gonzalo Fernandez <gonzaeze16.gf@gmail.com>
2023-06-01 11:33:49 -0300

    Inicializacion proyecto

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const songList = [
+    {
+        title: "Acoustic Breeze",
+        file: "acousticbreeze.mp3",
+        cover: "1.jpg",
+    },
+
+    {
+        title: "A New Benning",
+        file: "Anewbenning.mp3",
+        cover: "2.jpg",
+    },
+
+    {
+        title: "Creative Minds",
+        file: "creativeminds.mp3",
+        cover: "3.jpg",
+    }
+]
+
